b37e31fd7c4f420c286d56caa4f796a2f50576ce
makenchi <lucas.santos9322@gmail.com>
2023-06-01 21:40:30 -0300

    feat: criando controller

---
+++ CatNameGeneratorApi/CatNameGeneratorApi/Controllers/CatNameGenController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatNameGeneratorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatNameGenController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var names = new List<string>();
+            names.Add("Gabigol");
+            names.Add("Arrascaeta");
+            names.Add("Everton Ribeiro");
+
+            return names;
+        }
+    }
+}
